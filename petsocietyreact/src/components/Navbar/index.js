9bf3f59216fcafacc169530e8abea3fd9698c539
callyanne <callyannerchee@gmail.com>
2023-03-16 23:20:48 +0800

    added navbar component

--- petsocietyreact/src/components/Navbar/index.js
+++ petsocietyreact/src/components/Navbar/index.js
@@ -1,16 +1,25 @@
 import React from "react"; 
+import "./styles.css"
+import logo from "./assets/dog_logo.png"
  
 function Navbar() { 
   return ( 
-    <nav className="main-header navbar navbar-expand bg-white navbar-light"> 
-    <p>PetSociety</p>
-      <ul className="navbar-nav"> 
-        <li className="nav-item"> 
-          <a className="nav-link" data-widget="pushmenu" href="/"> 
-            <i className="fa fa-bars" /> 
-          </a> 
-        </li> 
-      </ul> 
+    <nav className="nav"> 
+    <img src={logo} class="logo"/>
+    <a href="/" className="site-title"> Pet Society </a>
+    <div class="wrapper">
+      <ul>
+        <li><a href="/searchSitter">Search Sitter</a></li>
+        <li><a href="/bookings">Bookings</a></li>
+        <li><a href="/meetandgreets">My Meet and Greets</a></li>
+        <li><a href="/services">Our Services</a></li>
+      </ul>
+
+      <ul>
+        <li class="right"><a href="/help">Help</a></li>
+        <li class="right"><a href="/logout">Logout</a></li>
+      </ul>
+    </div>
     </nav> 
   ); 
 } 
