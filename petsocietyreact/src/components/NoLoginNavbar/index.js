664e5bb0334215332ea2875a3a48dffaebf1ecf4
callyanne <callyannerchee@gmail.com>
2023-03-24 14:13:06 +0800

    resolved merge issue for NoLoginNavbar

--- petsocietyreact/src/components/NoLoginNavbar/index.js
+++ petsocietyreact/src/components/NoLoginNavbar/index.js
@@ -1,10 +1,7 @@
 import React from "react"; 
 import "./styles.css"
 import logo from "./assets/dog_logo.png"
-<<<<<<< HEAD
-=======
 import { Link } from "react-router-dom"
->>>>>>> master
  
 function NoLoginNavbar() { 
   return ( 
@@ -18,11 +15,7 @@ function NoLoginNavbar() {
       </ul>
 
       <ul>
-<<<<<<< HEAD
-        <li class="right"><a href="/signUp">Sign Up</a></li>
-=======
         <li class="right"><Link to="/signUp">Sign Up</Link></li>
->>>>>>> master
         <li class="right"><a href="/signIn">Sign In</a></li>
         <li class="right"><a href="/help">Help</a></li>
       </ul>
