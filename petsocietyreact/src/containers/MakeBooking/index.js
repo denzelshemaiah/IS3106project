ad85a856f176734df1255b936d6070b1b8ddfa3a
callyanne <callyannerchee@gmail.com>
2023-03-27 16:01:00 +0800

    added semi-functional MakeBookings page, adjusted Bookings page

--- petsocietyreact/src/containers/MakeBooking/index.js
+++ petsocietyreact/src/containers/MakeBooking/index.js
@@ -1,30 +1,145 @@
 import React, { useEffect, useState } from "react";
-import { DatePicker } from "reactstrap-date-picker";
-import Navbar from "../../components/Navbar";
+import DatePicker from "react-datepicker";
 import './style.css'
-import moment from "moment";
+import { Card, CardText, CardBody, CardSubtitle, Button, Label, Input, Form, FormGroup} from "reactstrap";
+import { faCloudSun, faHouseChimney, faRepeat, faSuitcase, faTaxi } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
+import moment from 'moment-timezone';
+import "react-datepicker/dist/react-datepicker.css";
+import Api from "../../helpers/Api";
 
 //page to view all bookings, follows a tab view
 function MakeBookings(props) {
-    const [service, setService] = useState("");
-    const sitterName = props.sitter
-    const [startDate, setStartDate] = useState(moment("1990-01-01 00:00:00"));
-    const [sitterId, setSitterId] = useState(0);
-    const [parentId, setParentId] = useState(0);
+    const [service, setService] = useState("daycare");
+    const sitter = props.sitter
+    const parent = props.parent
+    const [startDate, setStartDate] = useState(moment().tz('Asia/Singapore').startOf("day").toDate());
     const [cost, setCost] = useState(0);
     const [created, setCreated] = useState(moment());
     const [description, setDescription] = useState("");
     const [endDate, setEndDate] = useState(moment("1990-01-01 00:00:00").toDate());
+    const [repeat, setRepeat] = useState("once")
 
+    //options to choose repeats etc
     let optionButtons = "";
 
     //for the top bar stating service
     let serviceIcon = ""
+    let serviceText = ""
+    let repeatButtons = ""
+
+    if (service === "taxi") {
+        serviceText = "Pet Taxi"
+        serviceIcon = (
+            <FontAwesomeIcon icon={faTaxi} style={{float: "left", marginRight: "15px", height:"30px", width:"30px"}}/>
+        )
+    } else if (service === "daycare") {
+        serviceText = "Doggy Day Care"
+        serviceIcon = (
+            <FontAwesomeIcon icon={faCloudSun} style={{float: "left", marginRight: "15px", height:"30px", width:"30px"}}/>
+        )
+    } else if (service === "housesitting") {
+        serviceText = "House Sitting"
+        serviceIcon = (
+            <FontAwesomeIcon icon={faHouseChimney} style={{float: "left", marginRight: "15px", height:"30px", width:"30px"}}/>
+        )
+    } else if (service === "boarding") {
+        serviceText = "Boarding"
+        serviceIcon = (
+            <FontAwesomeIcon icon={faSuitcase} style={{float: "left", marginRight: "15px", height:"30px", width:"30px"}}/>
+        )
+    }
+
+    if (service === "daycare" || service ==="housesitting" || service==="boarding") {
+        repeatButtons = (
+            <>
+                <h5 style={{marginBottom : "3vh"}}>How often do you need {serviceText}?</h5>
+                <Button outline color="secondary" className={repeat === "once" ? "active" : ""} style={{width: "45%", margin:"10px"}} onClick={() => setRepeat("once")}> 
+                    <FontAwesomeIcon icon={faCalendarAlt} style={{float: "left", height:"30px", width:"30px"}}/> One Time</Button>{' '}
+                <Button outline color="secondary"  className={repeat === "weekly" ? "active" : ""} style={{width: "45%", margin:"10px"}} onClick={() => setRepeat("weekly")}>
+                <FontAwesomeIcon icon={faRepeat} style={{float: "left", height:"30px", width:"30px"}}/>Repeat Weekly</Button>{' '}
+            </>
+        )
+    }
+
+    const selectDates = (dates) => {
+        const[start, end] = dates
+        setStartDate(start);
+        setEndDate(end);
+    }
+
+    const createBookingSubmit = (form) => {
+        form.preventDefault();
+        //fetch the Api
+        Api.createBooking({
+            //call API func here
+        })
+    }
+
 
     return (
-        <div class="wrapper">
-            <h2 class="m-4 ml-5" id="contact-header"> Contact {sitterName} </h2>
-        </div>
+        <>
+            <div style={{width: "50vw", margin: "auto"}}>
+                <div style={{display: "block", marginTop:"5vh", marginBottom:"3vh"}}> 
+                    <h2 id="contact-header"> Contact {sitter} </h2>
+                </div>
+
+                <div style={{display: "block"}}>
+                    <Card style={{
+                    padding: 0,
+                    width: '100%',
+                    VerticalAlignCenter: true,
+                    marginBottom:"3vh"
+                    }}>
+                        <CardBody>
+                            <CardText>
+                                <span>
+                                    {serviceIcon}
+                                </span>
+                                <span>
+                                    <h5>
+                                        {serviceText} 
+                                    </h5>
+                                </span>
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                
+                <Form onSubmit={createBookingSubmit}>
+                    <div style={{display: "block", marginBottom:"3vh"}}>
+                        {repeatButtons}
+                    </div>
+
+                    <div style={{display: "block"}}>
+                        <h5 style={{ marginBottom:"3vh"}}> Please select the dates for {serviceText} </h5>
+                        <DatePicker
+                            dateFormat="dd/MM/yyyy"
+                            selected={startDate}
+                            onChange={selectDates}
+                            startDate={startDate}
+                            endDate={endDate}
+                            selectsRange
+                        />
+                    </div>
+
+                    <div style={{display: "block", marginBottom:"3vh", marginTop:"5vh"}}>
+                        <h5>
+                            Message
+                        </h5>
+                        <FormGroup>
+                            <Label for="exampleText">Share a little about your pet and why they'd have a great time with {sitter}</Label>
+                            <Input type="textarea" name="text" id="exampleText" style={{height: "20vh"}} onChange={setDescription}/>
+                        </FormGroup>
+                    </div>
+
+                    <div style={{display: "block"}}>
+                        <Button type="submit" style={{backgroundColor: "#e6e4f5", color: "black"}}> Book now </Button>
+                    </div>
+                </Form>
+            </div>
+        </>
     );
 }
 
