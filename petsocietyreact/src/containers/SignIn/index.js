a6a47c2cb48d05e95dbb0c3727d30b59920a2105
Andrea L <e0772610@u.nus.edu>
2023-04-08 17:23:06 +0800

    forgot password popup modal UI, remember to link userauthentication and updatePassword methods here

--- petsocietyreact/src/containers/SignIn/index.js
+++ petsocietyreact/src/containers/SignIn/index.js
@@ -1,77 +1,117 @@
-import React from 'react';
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+import React, { useState } from 'react';
+import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox, MDBModal, MDBModalDialog, MDBModalContent, MDBModalHeader, MDBModalTitle, MDBModalBody, MDBModalFooter } from 'mdb-react-ui-kit';
 import "./style.css";
 
 function SignIn() {
 
+  // remember to import User from SignUp so that password can be changed
+  const [forgotPasswordModal, setForgotPasswordModal] = useState(false);
+  const toggleShowForgotPassword = () => setForgotPasswordModal(!forgotPasswordModal);
+
   return (
-    <MDBContainer fluid className="p-3 my-5 h-custom">
+    <>
+      <MDBContainer fluid className="p-3 my-5 h-custom">
 
-      <MDBRow>
+        <MDBRow>
 
-        <MDBCol col='10' md='6'>
-          <img src="https://img.freepik.com/free-vector/cute-dogs-park-scene_24877-51193.jpg?w=996&t=st=1679651672~exp=1679652272~hmac=d7bdc505e0a2d856d8720a32d6aea325646619df4af95b19a0baaf3eda102e49" class="img-fluid" alt="Sample image" />
-        </MDBCol>
+          <MDBCol col='10' md='6'>
+            <img src="https://img.freepik.com/free-vector/cute-dogs-park-scene_24877-51193.jpg?w=996&t=st=1679651672~exp=1679652272~hmac=d7bdc505e0a2d856d8720a32d6aea325646619df4af95b19a0baaf3eda102e49" class="img-fluid" alt="Sample image" />
+          </MDBCol>
 
-        <MDBCol col='4' md='6'>
-          <h1>Welcome Back!</h1> 
-          <MDBInput wrapperClass='mb-4' 
-          label='Email' 
-          id='formControlLg' 
-          type='email' 
-          size="lg"/>
-          <MDBInput wrapperClass='mb-4' 
-          label='Password' 
-          id='formControlLg' 
-          type='password' 
-          size="lg"/>
+          <MDBCol col='4' md='6'>
+            <h1>Welcome Back!</h1>
+            <MDBInput wrapperClass='mb-4'
+              label='Email'
+              id='formControlLg'
+              type='email'
+              size="lg" />
+            <MDBInput wrapperClass='mb-4'
+              label='Password'
+              id='formControlLg'
+              type='password'
+              size="lg" />
 
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="#/ForgotPassword">Forgot password?</a>
+            <div className="d-flex justify-content-between mb-4">
+              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+              <a onClick={toggleShowForgotPassword}>Forgot password?</a>
+              <MDBModal show={forgotPasswordModal} setShow={setForgotPasswordModal} tabIndex='-1'>
+                <MDBModalDialog>
+                  <MDBModalContent>
+                    <MDBModalHeader>
+
+                      <MDBModalTitle>
+                        Forgot Password?
+                      </MDBModalTitle>
+
+                      <MDBBtn className='btn-close'
+                        color='none'
+                        onClick={toggleShowForgotPassword}>
+                      </MDBBtn>
+
+                    </MDBModalHeader>
+
+                    <MDBModalBody>
+                      <MDBInput label='Old Password' id='oldPassword' type='text'/>
+                      <div style={{ margin: '1rem 0' }}></div>
+                      <MDBInput label='New Password' id='newPassword' type='text'/> 
+                    </MDBModalBody>
+
+                    <MDBModalFooter>
+                      <MDBBtn color='secondary' 
+                      onClick={toggleShowForgotPassword}>
+                        Close
+                      </MDBBtn>
+                      <MDBBtn>Save changes</MDBBtn>
+                    </MDBModalFooter>
+
+                  </MDBModalContent>
+                </MDBModalDialog>
+
+              </MDBModal>
+            </div>
+
+            <div className='text-center text-md-start mt-4 pt-2'>
+              <MDBBtn className="mb-0 px-5"
+                size='lg'>Login
+              </MDBBtn>
+              <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account?
+                <a href="#/SignUp/1" className="link-danger"> Register</a></p>
+            </div>
+
+          </MDBCol>
+
+        </MDBRow>
+
+        <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+
+          <div className="text-white mb-3 mb-md-0" style = {{ color : 'indigo'}}>
+            Copyright Pet Society ©. All rights reserved.
           </div>
 
-          <div className='text-center text-md-start mt-4 pt-2'>
-            <MDBBtn className="mb-0 px-5" 
-            size='lg'>Login
+          <div>
+
+            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
+              <MDBIcon fab icon='facebook-f' size="md" />
             </MDBBtn>
-            <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? 
-             <a href="#/SignUp/1" className="link-danger"> Register</a></p>
+
+            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
+              <MDBIcon fab icon='twitter' size="md" />
+            </MDBBtn>
+
+            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
+              <MDBIcon fab icon='google' size="md" />
+            </MDBBtn>
+
+            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
+              <MDBIcon fab icon='linkedin-in' size="md" />
+            </MDBBtn>
+
           </div>
 
-        </MDBCol>
-
-      </MDBRow>
-
-      <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-
-        <div className="text-white mb-3 mb-md-0">
-          Copyright Pet Society ©. All rights reserved.
         </div>
 
-        <div>
-
-          <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-            <MDBIcon fab icon='facebook-f' size="md"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white'  }}>
-            <MDBIcon fab icon='twitter' size="md"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white'  }}>
-            <MDBIcon fab icon='google' size="md"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white'  }}>
-            <MDBIcon fab icon='linkedin-in' size="md"/>
-          </MDBBtn>
-
-        </div>
-
-      </div>
-
-    </MDBContainer>
+      </MDBContainer>
+    </>
   );
 }
 
